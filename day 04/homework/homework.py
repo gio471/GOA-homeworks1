6d25dcfc36a8d0294c7ecbd6a3b0381476ab5f24
gio471 <alavidzegiorgi509@gmail.com>
2024-09-19 10:20:16 -0700

    fifth commit

---
+++ day 04/homework/homework.py
@@ -0,0 +1,55 @@
+  # bug არის პროგრამული შეცდომა რომელიც იწვევს არასწორი შედეგის გამოტანას.ბაგი არის ადამიანის შეცდომით გამოწვეული ხარვეზი,დეფექტი.
+  # debugging არის პროცესი რომლის დროსაც ადამიანი ასწორებს თავისივე დაშვებულ შეცდომებს
+
+
+
+
+
+#goat age = 700   ეს კოდი არასწორია რადგან არ შეიძლება (space-ს გამოყენება)
+#goat_age = 700   შეიძლება იმიტომ რომ ეს არის snake case (სიტყვების გამოყობა ქვედა ტირით)
+
+
+
+
+
+print(1146869 * 20932)
+print(111 * 323)
+print(23 * 90)
+print(145 * 876)
+print(1430 * 323)
+
+print(4623330825 / 4695)
+print(1333320 / 120)
+print(90 / 3)
+print(11310/ 145)
+print(1430 / 55)
+
+print(1146869 + 20932)
+print(111 + 323)
+print(23 + 90)
+print(145 + 876)
+print(1430 + 323)
+
+print(1146869 - 20932)
+print(1111 - 323)
+print(230 - 90)
+print(1450 - 876)
+print(1430 - 323)
+
+
+my_age = 12
+older_sister_age = 10
+younger_sister_age = 5
+father_age = 42
+mother_age = 39
+grandfather_age = 81
+grandmother_age = 69
+
+print(my_age) 
+print(older_sister_age)
+print(younger_sister_age)
+print(father_age)
+print(mother_age)
+print(grandfather_age)
+print(grandmother_age)
+
