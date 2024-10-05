75063e594e8c7effe99aae9a46a8de88d10cfda1
gio471 <alavidzegiorgi509@gmail.com>
2024-10-05 08:17:45 -0700

    tenth commit

---
+++ day 09/classwork/classwork.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age:"))
+print(age // 18)  #თუ გამოიტანა 0 ესეიგი არ არის სრულწლოვანი სხვა ყველა შემთხვევაში არის
+name = input("enter your name:")
+print(name == "Giorgi")
+
+num = int(input("enter random number"))
+print(num % 2)
