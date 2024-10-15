d75387a8abadc966159aef5e4c32b4a6fc0ebbe2
gio471 <alavidzegiorgi509@gmail.com>
2024-10-15 07:40:15 -0700

    twelfth commit

---
+++ day 11/homework/homework.py
@@ -0,0 +1,24 @@
+one = True or True
+two =True or False
+three = False or False
+four = False or True
+five = True and True
+six = True and False
+seven = False and False
+eight = False and True
+
+age = int(input("enter your age:"))
+print(age > 18)
+name = input("Enter your name :")
+print(name == "Giorgi")
+heigt =int(input("Enter your heigt"))
+print(heigt > 180.0)
+
+
+#Flowchart - ფოტოს მეშვეობით ალგორითმის ასახვა
+
+#Pseudocode - ალგორითმის ასახვა ინგლისურით + პროგრამირების ენით 
+
+#Natural Language - ალგორითმის გამოსახვა ბუნებრივი ენით (ინგლისურით, ქართულით...)
+
+#ალგორითმი არის გზა რომელიც უნდა გაიარო ნაბიჯ ნაბიჯ მაგ:ტორტის რეცეპტი,ლეგო,პაზლი
