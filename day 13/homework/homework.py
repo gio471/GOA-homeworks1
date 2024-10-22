155b4521696342718735fb1a6ec28cdeec699be0
gio471 <alavidzegiorgi509@gmail.com>
2024-10-22 07:21:29 -0700

    fourtheenth

---
+++ day 13/homework/homework.py
@@ -0,0 +1,33 @@
+for i in range(11):
+    print(i)
+for i in range(50,101):
+    print(i)
+for i in range(200,500,2):
+    print(i)
+for i in range(1,50,2):
+    print(i)
+
+surname = input("Enter surname:")
+for i in surname:
+    print(i)
+
+
+#for loops გამოიყენება იმისათვის რომ გაიმეოროს ის დავალება რასაც მივცემთ და რამდენჯერაც მივუთითებთ იმდენჯერ
+
+num = 10
+while num < 16:
+    print(num)
+    num = num + 1
+
+num1 = 50
+while num1 > 0:
+    print(num1)
+    num1 = num1 - 1
+
+
+#while loop შეიძლება გამოიყენებოდეს მაშინ როდესაც არ იცი რამდენი იტერაცია დაგჭირდება
+for i in range(10,70,2):
+    print(i)
+
+for i in range(25,57,2):
+    print(i)
