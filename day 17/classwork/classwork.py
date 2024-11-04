1a847d7782465ebfb50fd617e803b516f6987958
gio471 <alavidzegiorgi509@gmail.com>
2024-11-04 09:20:56 -0800

    eightheenht commit

---
+++ day 17/classwork/classwork.py
@@ -0,0 +1,4 @@
+names = ["giorgi","nika","givi","dato","erekle","nini"",elene"]
+for i in names:
+    if "giorgi" == i:
+        print(i)
---
+++ day 17/homework/homework.py
@@ -0,0 +1,25 @@
+#words = ["a","b","c"]
+#print(words[0])
+#print(words[1])
+#print(words[2])
+
+#alphabet = ["a","b","c","d","e","f","g","h","i","j"]
+#for i in alphabet:
+#   print(i)
+
+
+#nums = [1,12,2,23,3,4]
+#for i in nums:
+#    if i > 10:
+#        print(i)
+
+random = ["giorgi",7,7.5,True,False,"vano",11]
+for i in range(1):
+    print(type (random [0]))
+    print(type (random [0]))
+    print(type (random [0]))
+    print(type (random [0]))
+    print(type (random [0]))
+    print(type (random [0]))
+    print(type (random [0]))
+
