07bd3f8f8f77a1a2f09e683f01bdf292c5f9bb3e
gio471 <alavidzegiorgi509@gmail.com>
2024-11-08 07:22:26 -0800

    eighteenth commit

--- day 17/homework/homework.py
+++ day 17/homework/homework.py
@@ -1,25 +1,21 @@
-#words = ["a","b","c"]
-#print(words[0])
-#print(words[1])
-#print(words[2])
+words = ["a","b","c"]
+print(words[0])
+print(words[1])
+print(words[2])
 
-#alphabet = ["a","b","c","d","e","f","g","h","i","j"]
-#for i in alphabet:
-#   print(i)
+alphabet = ["a","b","c","d","e","f","g","h","i","j"]
+for i in range(10):
+   print(alphabet[i])
+
+for i in alphabet:
+    print(i)
 
 
-#nums = [1,12,2,23,3,4]
-#for i in nums:
-#    if i > 10:
-#        print(i)
+nums = [1,12,2,23,3,4]
+for i in nums:
+    if i > 10:
+        print(i)
 
 random = ["giorgi",7,7.5,True,False,"vano",11]
-for i in range(1):
-    print(type (random [0]))
-    print(type (random [0]))
-    print(type (random [0]))
-    print(type (random [0]))
-    print(type (random [0]))
-    print(type (random [0]))
-    print(type (random [0]))
-
+for i in random:
+    print(type (i))
