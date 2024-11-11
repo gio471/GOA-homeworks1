758df2f5910b02d1fc8489a4760fc48826f56ac9
gio471 <alavidzegiorgi509@gmail.com>
2024-11-11 09:58:52 -0800

    twentieth commit

---
+++ day 19/classwork/classwork.py
@@ -0,0 +1,22 @@
+random = [1,2,3,4,5,6,7,8,9,10]
+for i in range(10):
+    if i % 2 != 0:
+        print(random[i])
+
+nums = [1,2,3,4,5]
+sum = 0
+for i in nums:
+    sum += i
+print(sum)
+    
+
+num = [1,2,3,4,5,6,7,8,9,10]
+for i in num:
+    if i % 2 == 0:
+        print(i,"luwia")
+    else:
+        print(i,"kentia")
+
+
+
+    
---
+++ day 19/homework/homework.py
@@ -0,0 +1,21 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+sum = 1
+for i in nums:
+    sum *= i
+print(sum)
+
+
+print(nums[0:8])
+
+
+num = [1,2,3,4,5]
+for i in num:
+    if i < 5:
+        print(i)
+
+sum = 0
+for i in nums:
+    sum += i
+print(sum)
+
+
