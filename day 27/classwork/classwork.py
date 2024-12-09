69c19a3c8c277b93e2b0cc126626d182528bc6fa
gio471 <alavidzegiorgi509@gmail.com>
2024-12-09 05:29:36 -0800

    the twenty-eighth commit

---
+++ day 27/classwork/classwork.py
@@ -0,0 +1,7 @@
+def sum(nums):
+    total = 0
+    for i in nums:
+        total += i
+    print(total)
+
+sum([1,2,3,4,56,9])
---
+++ day 27/homework/homework.py
@@ -0,0 +1,39 @@
+def sum(num1,num2):
+    print(num1 + num2)
+sum(1, 2)
+
+
+
+def num(num1):
+    print(num1)
+    if num1 % 2 == 0:
+        print(num1,"luwia")
+    elif num1 % 2 == 1:
+        print(num1,"kentia")
+num(5)
+
+
+def gio(num1):
+    if num1 > 0:
+        print(num1,"dadebita")
+    elif num1 < 0:
+        print(num1,"uaryofitia")
+gio(-1)
+
+
+
+
+def nia(giorgi):
+    print("hello",giorgi)
+
+nia("nia")
+nia("gia")
+
+
+
+
+str1 = input("Enter your name:  ")
+str2 = input("Enter your name:  ")
+def concatination(str1,str2):
+    print(str1 + str2)
+concatination(str1,str2)
