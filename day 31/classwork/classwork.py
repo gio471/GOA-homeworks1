e28551a9d8de72564a7ec73f608ad2b53136c8fc
gio471 <alavidzegiorgi509@gmail.com>
2024-12-20 10:21:21 -0800

    commit

---
+++ day 31/classwork/classwork.py
@@ -0,0 +1,31 @@
+#https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python:
+def solution(string):
+    return string[::-1]
+
+#https://www.codewars.com/kata/544675c6f971f7399a000e79/train/python:
+def string_to_number(s):
+    # your code here
+    return int(s)
+
+#https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python:
+def boolean_to_string(b):
+    #your code here
+    return str(b)
+
+#https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/python:
+def make_upper_case(s):
+    # Code here
+    return s.upper()
+
+#https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python:
+def bool_to_word(boolean):
+    # TODO
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+
+#https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python:
+def remove_char(s):
+    #your code here
+    return s[1;-1]
