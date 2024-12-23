792eb8eb3f083d25c8bbfe0cf327d69938f90a63
gio471 <alavidzegiorgi509@gmail.com>
2024-12-23 07:11:41 -0800

    commit

---
+++ day 31/homework/homework.py
@@ -0,0 +1,36 @@
+# https://www.codewars.com/kata/523b4ff7adca849afe000035/train/python:
+def greet():
+    return "hello world!"
+
+# https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python:
+def opposite(number):
+  # your solution here
+    return number-number-number
+
+# https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python:
+def make_negative( number ):
+    return -abs( number )
+
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python:
+def repeat_str(repeat, string):
+    return string * repeat
+
+# https://www.codewars.com/kata/515e271a311df0350d00000f/train/python:
+def square_sum(numbers):
+    return sum(i**2 for i in numbers)
+
+# https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/python:
+def find_smallest_int(arr):
+    return min(arr)
+
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python:
+def summation(num):
+     return sum(range(1, num + 1))
+
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python:
+def no_space(x):
+    return x.replace(" ", "")
+
+# https://www.codewars.com/kata/54edbc7200b811e956000556/train/python:
+def count_sheeps(sheep):
+    return sheep.count(True)
